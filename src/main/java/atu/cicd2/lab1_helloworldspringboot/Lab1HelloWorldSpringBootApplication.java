9273406d62ea869cdbc4ea92da1a36c8da4e2bca
ALLYN MC KENNA PATTERSON - STUDENT <g00392765@atu.ie>
2023-09-27 09:29:41 +0100

    First commit

---
+++ src/main/java/atu/cicd2/lab1_helloworldspringboot/Lab1HelloWorldSpringBootApplication.java
@@ -0,0 +1,13 @@
+package atu.cicd2.lab1_helloworldspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab1HelloWorldSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab1HelloWorldSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/atu/cicd2/lab1_helloworldspringboot/Lab1HelloWorldSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package atu.cicd2.lab1_helloworldspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab1HelloWorldSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
